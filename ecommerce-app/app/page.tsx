b7b25f34c0a7bc1886fa7084d02665d09202e3ca
Bakar31 <abubakar.siddik@sazim.io>
2023-11-15 23:53:45 +0600

    add navbar and cover image

--- ecommerce-app/app/page.tsx
+++ ecommerce-app/app/page.tsx
@@ -1,7 +1,28 @@
 import Image from 'next/image'
+import Products from './components/products'
+import Navbar from './components/navbar'
 
 export default function Home() {
   return (
-    <h1 className='text-center text-lg'>Hello from SazimStore!</h1>
+    // <h1 className='text-center text-lg'>Hello from SazimStore!</h1>
+    <div>
+      <div>
+        <Navbar/>
+      </div>
+      <div className='flex justify-center'>
+        <Image
+          src="/cover-photo.jpg"
+          width={700}
+          height={500}
+          alt="Cover photo" 
+          />
+      </div>
+
+      <div>
+        <Products/>
+      </div>
+    </div>
+    
+    
   )
 }
