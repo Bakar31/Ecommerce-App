9060d3f9c92da3ff2cacbd753dad97c985576acb
Bakar31 <abubakar.siddik@sazim.io>
2023-12-18 17:10:27 +0600

    add role column

--- server/routes/auth.ts
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-import passport from 'passport';
-
-// auth with google+
-router.get('/google', passport.authenticate('google', {
-    scope: ['profile']
-}));
